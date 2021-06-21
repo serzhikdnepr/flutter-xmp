a77d5ca678e229cfa73a2ae6e95595385c6c8b72
Queizy Sartori <queizydomingues@hotmail>
2021-06-21 18:47:10 -0300

    Code documentation added. Null-safety migration.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _xmpData = "Click the button!";
-  final url = "https://firebasestorage.googleapis.com/v0/b/constructin-15d0f.appspot.com/o/projects%2Fcaptures%2Fd7f2a7f4-a5b3-48f9-bfeb-69747f874d23.jpeg?alt=media&token=5fddf914-8a06-49a1-be67-350bb4ec98c5";
+  final url = "ADD-YOUR-URL-HERE";
 
   @override
   void initState() {
@@ -51,15 +51,11 @@ class _MyAppState extends State<MyApp> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.network(url),
-
               ElevatedButton(
                 child: Text("Get data!"),
                 onPressed: getImageXmp,
               ),
-
-              Text(
-                _xmpData
-              )
+              Text(_xmpData)
             ],
           ),
         ),
--- lib/flutter_xmp.dart
+++ lib/flutter_xmp.dart
@@ -1,47 +1,35 @@
-
 import 'dart:async';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
-enum UrlType {
-  local,
-  remote
-}
+/// Enum used to selct the provided URL type.
+enum UrlType { local, remote }
 
+/// A Flutter library to deal with Images XMP data extraction using native
+/// implementation for both Android and iOS.
 class FlutterXmp {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_xmp');
+  static const MethodChannel _channel = const MethodChannel('flutter_xmp');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-  static Future<Map<String, dynamic>> extractXMPFrom({
-    @required String url,
-    UrlType type = UrlType.remote
-  }) async {
-
+  /// Returns a [Map] with the extracted XMP data from a given URL.
+  /// The data extraction is made on the native side.
+  ///
+  /// Obs: Currently supports only [UrlType.remote].
+  static Future<Map<String, dynamic>> extractXMPFrom(
+      {required String url, UrlType type = UrlType.remote}) async {
     try {
-      if (url == null) {
-          throw(Exception("An URL must be provided"));
-      }
-
       if (type == UrlType.remote) {
-        final xmpData = await _channel.invokeMethod("extractXmpFromRemote", {
-          "url": url
-        });
+        final xmpData =
+            await _channel.invokeMethod("extractXmpFromRemote", {"url": url});
 
         return Map<String, dynamic>.from(xmpData);
       }
 
-      throw(UnimplementedError());
-    } catch(e, stack) {
+      throw (UnimplementedError());
+    } catch (e, stack) {
       print(e);
       print(stack);
 
-      throw(e);
+      throw (e);
     }
   }
 }
